5d63b0f85ed6e7b5d705c761ce8e335226789980
Natsec <kamilp@hotmail.fr>
2025-09-06 15:49:51 +0200

    v1.1 multiple mkv

--- SonicBoom.ps1
+++ SonicBoom.ps1
@@ -1,39 +1,51 @@
-$files = Get-ChildItem inputs/*.mkv
-foreach ($filepath in $files){
-    $filename = [System.IO.Path]::GetFileName($filepath)
+$inputs = Get-ChildItem inputs/*.mkv
+foreach ($filepath in $inputs){
+    $filename = [System.IO.Path]::GetFileNameWithoutExtension($filepath)
 
     $deco = "=" * $filename.length + "="
     Write-Host "$deco`n$filename`n$deco" -ForegroundColor green
 
-    $json = mkvmerge -J $filepath -v
-    # $json
-    $json = $json | ConvertFrom-Json
-    $json.tracks | ForEach-Object{
+    $json_metadata = mkvmerge -J $filepath
+    # $json_metadata
+    $metadata = $json_metadata | ConvertFrom-Json
+
+    Write-Host "Audio tracks :" -ForegroundColor green
+    $metadata.tracks | ForEach-Object{
         if ($_.type -eq "audio"){
             $id = $_.id
-            $type = $_.type
             $lang = $_.properties.language
-            $track_name = $_.properties.track_name.replace(':', '')
-            # AC-3 to AC3
-            $codec = $_.codec.replace('-', '')
+            $track_name = $_.properties.track_name
+            Write-Host "$id`:$track_name ($lang)" -ForegroundColor green
+        }
+    }
+
+    $tracklist = ""
+    $metadata.tracks | ForEach-Object{
+        if ($_.type -eq "audio"){
+            $id = $_.id
+            $lang = $_.properties.language
+            $track_name = $_.properties.track_name
+            $codec = $_.codec
 
             ""
             # Extracting audio track
-            Write-Host "Extracting audio track (id:$id lang:$lang name:`"$track_name`")" -ForegroundColor green
-            mkvextract $filepath tracks ${id}:"tmp/$id-$lang-$track_name.$codec"
-            ""
+            Write-Host "Extracting audio track $id`:$track_name ($lang)" -ForegroundColor green
+            mkvextract $filepath tracks ${id}:"tmp/$filename-$id-$lang-$track_name.$codec"
             # Compressing audio track
-            Write-Host "Compressing audio track" -ForegroundColor green
-            ffmpeg -hide_banner -y -i "tmp/$id-$lang-$track_name.$codec" -filter 'compand=0:1:-90/-900|-70/-70|-30/-9|0/-3:6:0:0:0' "tmp/$id-$lang-$track_name-compressed.mp3"
-            ""
+            Write-Host "Compressing audio track $id`:$track_name ($lang)" -ForegroundColor green
+            ffmpeg -hide_banner -y -i "tmp/$filename-$id-$lang-$track_name.$codec" -filter 'compand=0:1:-90/-900|-70/-70|-30/-9|0/-3:6:0:0:0' "tmp/$filename-$id-$lang-$track_name.compressed.mp3"
+
             # Building tracklist
-            $tracklist += "--language 0:$lang --track-name 0:`"_$lang-$track_name`" `"tmp/$id-$lang-$track_name-compressed.mp3`" "
+            $tracklist += "--language 0:$lang --track-name 0:`"Compressed $track_name`" `"tmp/$filename-$id-$lang-$track_name.compressed.mp3`" "
         }
     }
-    # Adding audio tracks to output file
-    Write-Host "Adding audio tracks to output file `"output/_$filename`"" -ForegroundColor green
-    Invoke-Expression "mkvmerge `"$filepath`" $tracklist -o `"output/_$filename`" --flush-on-close"
-    ""
-}
 
-# pause
+    ""
+    # Merging audio tracks to output file
+    Write-Host "Merging audio tracks in `"output/Compressed audio - $filename.mkv`"" -ForegroundColor green
+    Invoke-Expression "mkvmerge `"$filepath`" $tracklist -o `"output/Compressed audio - $filename.mkv`" --flush-on-close"
+    ""
+    ""
+
+    # pause
+}
